d66a596acbfc7761ced76e312c14cf83835f9b66
shelbyg2 <shelby02026@gmail.com>
2026-06-15 17:13:36 +0300

    Added the initial tmdb  response models

---
+++ app/src/main/java/tech/unrealistic/cineflix/data/remote/models/TmdbResponse.kt
@@ -0,0 +1,31 @@
+package tech.unrealistic.cineflix.data.remote.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MovieResponse (
+    val page: Int,
+    val  results : List<Movie>
+)
+
+@Serializable
+data class Movie (
+    val id : Int,
+    val title: String,
+    val  overview: String,
+    @SerialName("poster_path") val posterPath: String?,
+    @SerialName("backdrop_path") val backdropPath: String?,
+    @SerialName("release_date") val releaseDate: String?,
+    @SerialName("vote_average") val voteAverage: Double,
+    @SerialName("vote_count") val voteCount: Int,
+    @SerialName("genre_ids") val genreIds: List<Int>,
+    @SerialName("original_language") val originalLanguage: String,
+    @SerialName("original_title") val originalTitle: String,
+    val popularity: Double,
+    val video : Boolean = false,
+
+
+)
+
+
