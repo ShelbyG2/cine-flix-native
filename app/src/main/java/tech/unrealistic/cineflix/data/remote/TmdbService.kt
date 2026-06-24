a47cadf80cafaf3f5b912e7f13dd7ab6af3252e1
shelbyg2 <shelby02026@gmail.com>
2026-06-24 23:48:12 +0300

    Added daily poppular films endpoint

--- app/src/main/java/tech/unrealistic/cineflix/data/remote/TmdbService.kt
+++ app/src/main/java/tech/unrealistic/cineflix/data/remote/TmdbService.kt
@@ -3,17 +3,26 @@ package tech.unrealistic.cineflix.data.remote
 
 import retrofit2.http.GET
 import retrofit2.http.Query
+import tech.unrealistic.cineflix.data.remote.models.MediaItem
+import tech.unrealistic.cineflix.data.remote.models.Movie
 import tech.unrealistic.cineflix.data.remote.models.TmdbResponse
+import tech.unrealistic.cineflix.data.remote.models.Tv
 
 interface TmdbService {
-    @GET("movie/popular")
-    suspend fun getPopularMovies(
-        @Query("language") language: String = "en-US",
-        @Query("page") page: Int =1
-    ) : TmdbResponse
     @GET("trending/movie/week")
     suspend fun getTrendingMovies(
+        @Query("language") language: String = "en-US",
+        @Query("page") page: Int =1
+    ) : TmdbResponse<Movie>
+    @GET("trending/tv/week")
+    suspend fun getTrendingShows(
         @Query("language") language: String= "en-US",
         @Query("page") page: Int =1
-    ) : TmdbResponse
+    ) : TmdbResponse<Tv>
+
+    @GET("popular/all/day")
+    suspend fun getPopularShows(
+        @Query("language") language: String= "en-US",
+        @Query("page") page: Int =1
+    ) : TmdbResponse<MediaItem>
 }
