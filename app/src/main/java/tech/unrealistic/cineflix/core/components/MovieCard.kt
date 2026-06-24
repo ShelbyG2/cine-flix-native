f602edaa97de89f8f2a8c7e348dec79a6a990dad
shelbyg2 <shelby02026@gmail.com>
2026-06-24 22:06:55 +0300

    Added overlay for the media section cards

--- app/src/main/java/tech/unrealistic/cineflix/core/components/MovieCard.kt
+++ app/src/main/java/tech/unrealistic/cineflix/core/components/MovieCard.kt
@@ -2,13 +2,10 @@ package tech.unrealistic.cineflix.core.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -19,68 +16,67 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import tech.unrealistic.cineflix.data.remote.models.Movie
+import tech.unrealistic.cineflix.data.remote.models.Tv
 
-
+interface media <T>
 @Composable
-fun MovieCard(modifier: Modifier = Modifier, movie: Movie
+fun MediaCard(modifier: Modifier = Modifier,
+              movie: Movie?= null,
+              tvShow: Tv?= null,
+              hero: Boolean? = null
 ) {
-    Card( modifier= Modifier.width(150.dp).height(250.dp)) {
-        Box(modifier= Modifier.fillMaxSize(),
-            contentAlignment = Alignment.BottomCenter
+    val isHero = hero?: false
+
+         val posterPath = movie?.posterPath ?: tvShow?.posterPath
+         val displayTitle = movie?.title ?: tvShow?.name
+    Box(
+        contentAlignment = Alignment.BottomCenter,
+        modifier= if (isHero){
+            Modifier.fillMaxSize()
+        }else{Modifier.width(140.dp) }
+    ) {
+        Card(
+            modifier = Modifier.fillMaxSize()
         ) {
-            val posterUrl = "https://image.tmdb.org/t/p/w500${movie.posterPath}"
+
+
+            val posterUrl = "https://image.tmdb.org/t/p/w500$posterPath"
             AsyncImage(
                 model = posterUrl,
-                contentDescription = movie.title,
-                modifier= Modifier
-                    .fillMaxSize()
-                    .aspectRatio(3f/5f),
-                contentScale = ContentScale.FillBounds
-            )
-            // Gradient overlay
-            Box(
+                contentDescription = displayTitle,
+
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(100.dp) // height of gradient area
-                    .align(Alignment.BottomCenter)
-                    .background(
-                        Brush.verticalGradient(
-                            colors = listOf(Color.Transparent, Color.Black),
-                            startY = 0f,
-                            endY = Float.POSITIVE_INFINITY
-                        )
-                    )
+                    .fillMaxSize()
+                    .aspectRatio(2f / 3f),
+                contentScale = ContentScale.Crop
             )
 
-            Text(
-                text = movie.title,
-                style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold),
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                color = MaterialTheme.colorScheme.onSurface
-            )
         }
-//        Column(
-//            modifier = Modifier
-//                .padding(horizontal = 8.dp, vertical = 10.dp)
-//        ) {
-//            // Movie Title
-//
-//
-//            Spacer(modifier = Modifier.height(4.dp))
-//
-//            // Release Year / Subtext
-//            val releaseYear = movie.releaseDate?.split("-")?.firstOrNull() ?: "Unknown"
-//            Text(
-//                text = releaseYear,
-//                style = MaterialTheme.typography.bodySmall,
-//                color = MaterialTheme.colorScheme.onSurfaceVariant
-//            )
-//        }
+        Box(
+            modifier = Modifier.fillMaxWidth()
+                .fillMaxHeight(0.5f)
+                .align(Alignment.BottomCenter)
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors=listOf(
+                            Color.Transparent,
+                            MaterialTheme.colorScheme.background.copy(0.95f))
+                         ))
+        )
+        Text(
+            text = displayTitle ?: "Unknown title ",
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            color = MaterialTheme.colorScheme.onSurface,
+
+
+        )
     }
+
+
+
 }
