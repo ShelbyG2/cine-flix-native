7d70d666bcbf921d812dfe209d98c1791238c33f
shelbyg2 <shelby02026@gmail.com>
2026-07-01 00:35:31 +0300

    Fix: 1. Removed the scaffold and top bar. 2. gave the Hero section a Height to take exactly 40% of the screen height

--- app/src/main/java/tech/unrealistic/cineflix/feature/home/HomeScreen.kt
+++ app/src/main/java/tech/unrealistic/cineflix/feature/home/HomeScreen.kt
@@ -6,7 +6,6 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -14,7 +13,6 @@ import androidx.compose.material.icons.twotone.WifiOff
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.carousel.rememberCarouselState
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -25,10 +23,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import tech.unrealistic.cineflix.core.components.CustomTopBar
 import tech.unrealistic.cineflix.core.components.MediaDetailsBottomSheet
 import tech.unrealistic.cineflix.feature.home.components.HeroSection
 import tech.unrealistic.cineflix.feature.home.components.MediaSection
@@ -41,6 +40,11 @@ import tech.unrealistic.cineflix.feature.home.components.MediaSection
 fun HomeScreen(
     onNavigate: (() -> Unit)? = null, viewModel: HomeViewModel = viewModel()
 ) {
+    val context = LocalContext.current
+    val configuration = LocalConfiguration.current
+    val screenHeight =  configuration.screenHeightDp.dp
+    val carouselHeight = screenHeight * 0.4f
+
     // collectAsStateWithLifecycle stops collecting when the screen is in the background
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     var isMediaDetailsSheetOpen by remember { mutableStateOf(false) }
@@ -59,17 +63,12 @@ fun HomeScreen(
 
     }
 
-    Scaffold(
-        topBar = {
-            CustomTopBar(
-                title = "CineFlix", onSearchClick = { onNavigate?.invoke() })
-        }, containerColor = MaterialTheme.colorScheme.background
-    ) { innerPadding ->
+
 
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding),
+            ,
             contentAlignment = Alignment.Center
         ) {
             when (val state = uiState) {
@@ -112,7 +111,9 @@ fun HomeScreen(
                             items = state.trendingMixed,
                             carouselState = carouselState,
                             modifier = Modifier,
-                            topPadding = 10.dp
+                            topPadding = 10.dp,
+                            carouselHeight = carouselHeight,
+                            onMediaClick = openMediaDetails
                         )
 
                         MediaSection(
@@ -147,7 +148,7 @@ fun HomeScreen(
             }
         }
     }
-}
+
 
 
 
