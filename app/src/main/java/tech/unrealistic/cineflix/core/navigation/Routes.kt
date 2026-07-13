7cee634496801a059ee9f77e5efea077aad31580
shelbyg2 <shelby02026@gmail.com>
2026-07-13 19:42:09 +0300

    Feat: Added navigation routes for media details and player
    - Added `MediaDetailModal` data class to `Routes.kt` to handle navigation with `valid` and `type` parameters.
    - Added `Player` data class to support navigation to the player screen using `id` and `type`.
    - Standardized formatting by placing `@Serializable` annotations on separate lines for existing route objects.

--- app/src/main/java/tech/unrealistic/cineflix/core/navigation/Routes.kt
+++ app/src/main/java/tech/unrealistic/cineflix/core/navigation/Routes.kt
@@ -4,7 +4,15 @@ import androidx.navigation3.runtime.NavKey
 import kotlinx.serialization.Serializable
 
 
-@Serializable data object Home : NavKey
-@Serializable data object Discover : NavKey
-@Serializable data object Profile : NavKey
-@Serializable data object Favourites: NavKey
+@Serializable
+data object Home : NavKey
+@Serializable
+data object Discover : NavKey
+@Serializable
+data object Profile : NavKey
+@Serializable
+data object Favourites : NavKey
+@Serializable
+data class MediaDetailModal(val valid: Int, val type: String) : NavKey
+@Serializable
+data class Player(val id: Int, val type: String)
